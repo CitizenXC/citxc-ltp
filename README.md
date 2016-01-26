efbfd1f803c596e2422805f6ceec6e747f0e1a3b
Daniel E. Wheeler <daniel.e.wheeler@gmail.com>
2016-01-26 17:02:54 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# citxc-ltp
---
+++ calc.rb
@@ -0,0 +1,5 @@
+puts 1+2
+puts "Hours in a year: #{365.25 * 24}"
+puts "Minutes in a decade: #{365.25 * 10 * 24 * 60 }"
+puts "Age in seconds: #{365.25 * 24 * 3600 * (43 + (1.0/6.0))}"
+puts "Author's age: #{1160000000.0/1}"
